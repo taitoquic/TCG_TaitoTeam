c094c9972fdd1d6144af6452dd56772a69ef8604
QuicoGonzalez <quicoconqyconc@hotmail.com>
2021-01-25 13:58:53 +0100

    card structure, materials en gitignore

---
+++ Scripts/CardCode/Prayer.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Card/Magic/Prayer", order = 1)]
+public class Prayer : MagicEssence
+{
+
+}
---
+++ Scripts/CardCode/Spell.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Card/Magic/Spell", order = 0)]
+public class Spell : MagicEssence
+{
+
+}
